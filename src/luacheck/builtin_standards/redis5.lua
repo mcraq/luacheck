ca38bfb26f15b2f4c29b6fc7c4847f338ca38b85
Michael Crawford <michael.crawford@qlik.com>
2024-08-23 13:55:42 -0400

    Add support for redis built-ins

---
+++ src/luacheck/builtin_standards/redis5.lua
@@ -0,0 +1,68 @@
+local empty = {}
+
+local redis = {
+    fields = {
+        ARGV = {
+            other_fields = true,
+        },
+        KEYS = {
+            other_fields = true,
+        },
+
+        redis = {
+            fields = {
+                call = empty,
+                pcall = empty,
+                error_reply = empty,
+                status_reply = empty,
+                sha1hex = empty,
+                log = empty,
+                set_repl = empty,
+                replicate_commands = empty,
+                breakpoint = empty,
+                debug = empty,
+            },
+        },
+
+        cjson = {
+            fields = {
+                decode = empty,
+                encode = empty,
+            },
+        },
+
+        struct = {
+            fields = {
+                pack = empty,
+                unpack = empty,
+                size = empty,
+            }
+        },
+
+        cmsgpack = {
+            fields = {
+                pack = empty,
+                unpack = empty,
+            }
+        },
+
+        bit = {
+            fields = {
+                tobit = empty,
+                tohex = empty,
+                bnot = empty,
+                bor = empty,
+                band = empty,
+                bxor = empty,
+                lshift = empty,
+                rshift = empty,
+                arshift = empty,
+                rol = empty,
+                ror = empty,
+                bswap = empty,
+            }
+        }
+    }
+}
+
+return redis
